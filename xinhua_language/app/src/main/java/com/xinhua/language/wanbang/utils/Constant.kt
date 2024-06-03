4b91eac187a710503364c32bd6b5e2413aacd6e1
zhoujingjin <513784502@qq.com>
2024-06-03 18:07:12 +0800

    增加一点小东西

--- xinhua_language/app/src/main/java/com/xinhua/language/wanbang/utils/Constant.kt
+++ xinhua_language/app/src/main/java/com/xinhua/language/wanbang/utils/Constant.kt
@@ -5,8 +5,8 @@ date:2022/11/27
  */
 class Constant {
     companion object {
-        const val URL_PRIVACY_POLICY = "https://xinhuaapi.cpdtlp.com.cn/dict/user/privacy-policy.html"
-        const val URL_TERMS_OF_USE = "https://xinhuaapi.cpdtlp.com.cn/dict/user/privacy-policy.html"
+        const val URL_PRIVACY_POLICY = "https://xinhuaapi.cpdtlp.com.cn/dict/user/xiandai/privacy-policy-xiandai.html"
+        const val URL_TERMS_OF_USE = "https://xinhuaapi.cpdtlp.com.cn/dict/user/xiandai/user-agreement-xiandai.html"
         const val PRICE = "35"
         const val TYPE = "xinhua"
         const val DICT_ID = "xiandai"
