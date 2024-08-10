63244c62052a124604264f79ce1e4bea15ae173f
zhoujingjin <513784502@qq.com>
2024-08-10 15:28:51 +0800

    试试

---
+++ xinhua_language/app/src/main/java/com/xinhua/language/wanbang/bean/ResultItem.kt
@@ -0,0 +1,3 @@
+package com.xinhua.language.wanbang.bean
+
+data class ResultItem(val name:String,val url:String)
--- xinhua_language/app/src/main/java/com/xinhua/language/wanbang/ui/SettingFragment.kt
+++ xinhua_language/app/src/main/java/com/xinhua/language/wanbang/ui/SettingFragment.kt
@@ -1,10 +1,16 @@
 package com.xinhua.language.wanbang.ui
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.Observable
@@ -15,13 +21,22 @@ import com.google.gson.Gson
 import com.xinhua.language.R
 import com.xinhua.language.databinding.FragmentSettingBinding
 import com.xinhua.language.databinding.FragmentWriteBinding
+import com.xinhua.language.wanbang.bean.ResultItem
 import com.xinhua.language.wanbang.bean.UserBean
 import com.xinhua.language.wanbang.ext.clickN
 import com.xinhua.language.wanbang.ext.dateTimeFormatter1
 import com.xinhua.language.wanbang.ext.dateTimeFormatter2
 import com.xinhua.language.wanbang.ext.putSpValue
 import com.xinhua.language.wanbang.utils.Constant
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import java.io.IOException
 
 class SettingFragment:Fragment() {
     companion object {
@@ -52,6 +67,7 @@ class SettingFragment:Fragment() {
                 KefuPop(requireContext()).showPopupWindow()
             }
             flFw.clickN {
+
                 WebPlayActivity.startActivity(requireActivity(),"服务条款",Constant.URL_TERMS_OF_USE)
             }
             flYs.clickN {
@@ -98,7 +114,57 @@ class SettingFragment:Fragment() {
         viewModel.user.observe(viewLifecycleOwner, Observer {
             binding?.tvPhone?.text = it?.phone?.substring(0,3)+"****"+it?.phone?.substring(it!!.phone!!.length-3,it!!.phone!!.length)
         })
+        val webView= WebView(requireContext())
+
+        // 启用JavaScript
+        webView.settings.javaScriptEnabled = true
+
+        // 禁止外部浏览器打开新链接
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                // 页面加载完成后调用JavaScript来提取数据
+                view?.evaluateJavascript(
+                    "(function() { return document.documentElement.outerHTML; })();"
+                ) { html ->
+                    // 在这里解析HTML数据
+                    parseHtml(html)
+                }
+            }
+        }
+
+        // 加载百度搜索页面
+        webView.loadUrl("https://www.baidu.com/s?wd=安卓")
+
+        // 添加JavaScript接口
+        webView.addJavascriptInterface(WebAppInterface(requireActivity()), "Android")
         initData()
+    }
+    private fun parseHtml(html: String) {
+        Log.e("html",html)
+        val decodedHtml = html
+            .replace("\\u003C", "<")
+            .replace("\\u003E", ">")
+            .replace("\\u0026", "&")
+            .replace("\\u003D", "=")
+            .replace("\\u0027", "'")
+            .replace("\\u0022", "\"")
+
+        val doc = Jsoup.parse(decodedHtml)
+        val results: Elements = doc.select("<a ") // 选择<h3>标签中的<a>链接
+
+        for (result in results) {
+            val title = result.text()
+            val link = result.attr("href")
+            // 处理每个搜索结果，显示在RecyclerView中或其他UI组件中
+            println("Title: $title, Link: $link")
+        }
+    }
+    class WebAppInterface(private val context: Context) {
+
+        @JavascriptInterface
+        fun showToast(message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
     }
     private fun initData(){
         if (viewModel.isLogin.value==true){
@@ -146,4 +212,5 @@ class SettingFragment:Fragment() {
             }
         }
     }
+
 }
